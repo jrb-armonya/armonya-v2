bff4d0c0172f1d2802965cfc87cff2439231a962
jrb-armonya <j.youssef@armonya.fr>
2019-10-11 12:54:09 +0100

    some fixe in mes-fiches

--- resources/views/app/fiches/index-mes-fiches.blade.php
+++ resources/views/app/fiches/index-mes-fiches.blade.php
@@ -12,12 +12,16 @@
 
 @section('content')
     
+    @if($title === "Mes Rappels")
+    <h3>Mes Rappels</h3>
+    @else
     <h3>Vous pouvez consulter que les fiches du mois en cours</h3>
+    @endif
     <div class="card card-shadow mb-4" style="border: 1px solid #53c4fb;">
         <div class="card-header border-0">
             <div class="custom-title-wrap">
                 <div class="custom-title" style="font-size: 20px; ">
-                    {{ $title }} <span>{{$fiches->count()}}</span>
+                    {{ $title }} <span> : {{$fiches->count()}} </span>
                 </div>
             </div>
         </div>
@@ -42,7 +46,23 @@
                             <td>{{$fiche->h_rv}}</td>
                             <td><span class="badge badge-status" style="background-color: #{{$fiche->status->color}};">{{$fiche->status->name}}</span></td>
                             <td>{{$fiche->created_at->format('d/m/Y h:m')}}</td>
-                            <td><i class="ti-search btn-open-fiche" data-id="{{$fiche->id}}"></i></a></td>
+
+                            <td>
+                                @if($title == "Mes Rappels")
+                                <button 
+                                    type="button" 
+                                    class="btn btn-link form-pill btn-action open-btn"> 
+                                <i class="ti-search btn-open-fiche" data-id="{{$fiche->id}}"></i>
+                                </button>
+                                <button 
+                                    type="button"
+                                    class="btn btn-link form-pill btn-action delete-btn"
+                                    data-id={{ $fiche->id }}
+                                    data-toggle="modal" data-target="#fiche-delete" data-whatever="@mdo"> 
+                                    <i class="fa fa-trash icon-action-red"></i>
+                                </button>
+                                @endif
+                            </td>
                         </tr>
                         @endforeach
                         
@@ -51,6 +71,8 @@
             </div>
         </div>
     </div>
+
+    {{-- OPEN FICHE MODAL --}}
    <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true" data-backdrop="static" id="fiche-modal">
         <div class="modal-dialog modal-lg"  style="width: 90%; box-sizing: border-box; box-shadow: 0px -1px 2px -2px black;">
             <div class="modal-content">
@@ -67,6 +89,30 @@
             </div>
         </div>
     </div>
+
+    {{-- DELETE MODAL --}}
+    <div class="modal fade" id="fiche-delete" tabindex="-1" role="dialog" aria-labelledby="FicheModalDeleteLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="FicheModalDeleteLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+                <div class="modal-body text-center">
+                    <h1><i class="fa fa-warning" style="color: #f3516b;"></i></h1>
+                    <p>Etes-vous sûr de vouloir supprimer votre Rappel?</p>
+                    <h3 id="fiche-to-delete">  </h3>
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                    <input type="submit" class="btn btn-danger" value="Supprimer" id="delete">
+                </div>
+        </div>
+    </div>
+</div>
 @endsection
 @section('javascript')
 <script src="{{ asset('/backend/assets/vendor/input-mask/jquery.inputmask.bundle.js') }}"></script>
@@ -85,4 +131,5 @@
 <script src="{{ asset('/backend/app/fiches/open-fiche.js') }}"></script>
 <script src="{{ asset('/backend/app/fiches/populate-modal.js') }}"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/clockpicker/0.0.7/jquery-clockpicker.min.js"></script>
+<script src="{{asset('/backend/app/fiches/delete.js')}}"></script>
 @endsection

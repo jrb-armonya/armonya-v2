d1e986b4317c324844c37a239695237933c1778b
jrb-armonya <j.youssef@armonya.fr>
2019-10-11 13:15:06 +0100

    ipCheck need to be present on origin/master

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'ipCheck']], function () {
     // dashboard
     Route::get('/dashboard', 'DashboardController@index');
     // POST DATATABLE

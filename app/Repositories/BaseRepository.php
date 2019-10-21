6ca8fec5c27ff275c43d3d2964206d34d56bcdff
LZ-ouday <o.lazaar@armonya.fr>
2019-10-21 12:09:52 +0100

    BaseRepository, FichesRepository, ReportRepository

--- app/Repositories/BaseRepository.php
+++ app/Repositories/BaseRepository.php
@@ -13,10 +13,65 @@ class BaseRepository
         $this->model = $model;
     }
 
+    /**
+     * Get by date and attribut has default value created_at
+     *
+     * @param int $day
+     * @param int $month
+     * @param int $year
+     * @param string $attr
+     * @return void
+     */
+    public function byDate($day=null, $month=null, $year=null, $attr='created_at')
+    {
+        
+        $this->query->whereNotNull($attr);
+
+		if($day){
+			$this->query->whereDay($attr, $day);
+		}
+
+		if($month){
+			$this->query->whereMonth($attr,$month);
+		}
+
+		if($year){
+			$this->query->whereYear($attr, $year);
+		}
+		return $this;
+    }
+
+    public function execute()
+    {
+        return $this->query->get();
+    }
+
     // all
-    // get
+    public function all()
+    {
+        $this->query->where('created_at', '!=', null);
+        return $this;
+    }
     // getBy
+    public function getBy($attr ,$value)
+    {
+        $this->query->where($attr,$value);
+        return $this;
+    }
+
     // create
+    public function create($data)
+    {
+        return $this->query->create($data);
+    }
     // update
+    public function update($attr, $value, $data)
+    {
+        return $this->query->getBy($attr ,$value)->update($data);
+    }
     // delete
+    public function delete($attr, $value)
+    {
+        return $this->query->getBy($attr, $value)->delete();
+    }
 }
--- app/Repositories/Fiches/FichesRepository.php
+++ app/Repositories/Fiches/FichesRepository.php
@@ -28,19 +28,9 @@ class FichesRepository extends BaseRepository{
 	 * @param boolean $agent
 	 * @return QueryBuilder
 	 */
-    public function created($day = null, $month = null, $year = null, $excludeRappel = true, $agent = true)
+    public function created($excludeRappel = true, $agent = true)
     {
-		if($day){
-			$this->query->whereDay('created_at', $day);
-		}
-
-		if($month){
-			$this->query->whereMonth('created_at',$month);
-		}
-
-		if($year){
-			$this->query->whereYear('created_at', $year);
-		}
+		$this->query->where('created_at', '!=', null);
 
 		// exclude the rappel status
 		if($excludeRappel){
@@ -51,7 +41,7 @@ class FichesRepository extends BaseRepository{
 			$this->getOnlyAgent();
 		}
 
-		return $this->query;
+		return $this;
 	}
 
 	/**
@@ -78,7 +68,74 @@ class FichesRepository extends BaseRepository{
 	
 	
 	// Fiche par statut (A Ecouter, A Confirmer)
-	// Fiche Ecouted
-	// Env
-	// Repo
+	public function getByStatut($status_id)
+	{
+		$this->query->where('status_id', $status_id);
+		return $this;
+	}
+ 
+
+	// Fiche Action 
+	public function getByAction($action, $day = null, $month = null, $year = null)
+	{
+		if($day == null && $month == null && $year == null){
+			$this->query->whereNotNull($action);
+		}
+		$this->byDate($day, $month, $year, $action);
+		return $this;
+	}
+
+
+	// Fiches Archiver
+	public function getOnlyArchiver()
+	{
+		 $this->query->where('is_archived', 1);
+		 return $this;
+	}
+
+	//Fiches novalider
+	public function noValider()
+	{
+		 $this->query->whereIn('status_id',config('status.noValid'));
+		 return $this;
+	}
+
+	//Fiches valide aprés l'ecoute	
+	public function validAfterEcoute()
+	{
+		$this->query->where('valid_after_ecoute',1);
+		$this->getOnlyAgent();
+		return $this;
+	}  
+	
+	//Fiches par partenaire
+	public function getByPartenaire($partenaire_id)
+	{
+		 $this->query->where('partenaire_id',$partenaire_id);
+		 return $this;
+		
+	}
+
+	//Fiches par user function
+	public function getByUser( $user_id , $fun_id = 'user_id')
+	{
+		 $this->query->where($fun_id,$user_id);
+		 return $this;
+	}
+
+	
+   
+	//Fiches par groups
+	public function getByGroup($group)
+	{
+		$this->query->whereHas('user', function($q) use ($group){
+			$q->whereHas('groups', function($q) use ($group){
+				$q->where('group_id', $group);
+			});
+	    });
+		return $this;
+	}
+
+	
+
 }

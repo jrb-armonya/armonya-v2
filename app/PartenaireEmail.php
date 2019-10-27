79090555c7028b4ec8bd36aa2dbfec5137447cb4
jrb <jrb.youssef@gmail.com>
2019-10-27 14:36:49 +0100

    PartenaireEmail Model with relations.

--- app/PartenaireEmail.php
+++ app/PartenaireEmail.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class PartenaireEmail extends Model
 {
-    public $fillable = ['email', 'partenaire_id'];
+    public $fillable = ['email', 'partenaire_id', 'user_id'];
+
+    public function partenaire()
+    {
+        return $this->belongsTo(Partenaire::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }

5a33b8f4c9f894b13600d59adddcd59bd0d181fc
jrb <jrb.youssef@gmail.com>
2019-10-27 02:28:50 +0100

    Send email to the new Partenaire User function.

--- app/Http/Controllers/Partenaires/PartenaireHelper.php
+++ app/Http/Controllers/Partenaires/PartenaireHelper.php
@@ -9,10 +9,11 @@ use App\Mail\FicheConfirmed;
 use App\Fiche;
 use App\Partenaire;
 use App\EmailPartenaire;
+use App\PartenaireEmail;
 
 class PartenaireHelper extends Controller
 {
-    
+
     /**
      * sendEmailConfirmation
      * Envoi un email de confirmation au Partenaire
@@ -28,21 +29,18 @@ class PartenaireHelper extends Controller
 
         Mail::send('layouts.emails.rdv-confirmed', ['fiche' => $fiche], function ($m) use ($fiche, $path, $partenaire_emails, $partenaire_emails_cc) {
 
-            $subject = "CONFIRMATION de votre RDV pour le " . $fiche->d_rv->format('d/m/Y') . " " . $fiche->h_rv . ". avec " .ucwords($fiche->name) . " ". ucwords($fiche->prenom);
-            
+            $subject = "CONFIRMATION de votre RDV pour le " . $fiche->d_rv->format('d/m/Y') . " " . $fiche->h_rv . ". avec " . ucwords($fiche->name) . " " . ucwords($fiche->prenom);
+
             $m->from(env('MAIL_USERNAME'), 'Votre Rendez-vous');
             $m->attach($path);
             // Partenaire mail
-            foreach($partenaire_emails as $email_id)
-            {
+            foreach ($partenaire_emails as $email_id) {
                 $email = EmailPartenaire::find($email_id);
                 $m->to($email->email, $email->partenaire->name)->subject($subject);
             }
             // mail_cc
-            if($partenaire_emails_cc != '')
-            {
-                foreach($partenaire_emails_cc as $email_id_cc)
-                {
+            if ($partenaire_emails_cc != '') {
+                foreach ($partenaire_emails_cc as $email_id_cc) {
                     $email = EmailPartenaire::find($email_id_cc);
                     $m->cc($email->email);
                 }
@@ -61,21 +59,31 @@ class PartenaireHelper extends Controller
     {
         $partenaire = Partenaire::find($fiche->partenaire_id);
 
-        Mail::send('layouts.emails.rdv-annulation', ['fiche' => $fiche], function($m) use ($fiche, $partenaire) {
-                
-            $subject = "AANULATION de votre RDV pour le " . $fiche->d_rv->format('d/m/Y') . ".  " .ucwords($fiche->name) . " ". ucwords($fiche->prenom);
+        Mail::send('layouts.emails.rdv-annulation', ['fiche' => $fiche], function ($m) use ($fiche, $partenaire) {
+
+            $subject = "AANULATION de votre RDV pour le " . $fiche->d_rv->format('d/m/Y') . ".  " . ucwords($fiche->name) . " " . ucwords($fiche->prenom);
 
             $m->from('MAIL_USERNAME', 'Votre Rendez-vous');
-            foreach ($partenaire->emails as $email)
-            {
+            foreach ($partenaire->emails as $email) {
                 $m->to($email->email, $partenaire->name)->subject($subject);
             }
             // $m->bcc('infos@armonya.fr');
         });
     }
 
-   
+    public function sendEmaiLConfirmationEspacePartenaire($partenaire_id, $email_id, $password)
+    {
+        $partenaire = Partenaire::find($partenaire_id);
+        $email = PartenaireEmail::find($email_id);
 
-
-    
+        Mail::send(
+            'espace-partenaire::layouts.email-confirmation-espace',
+            ['partenaire' => $partenaire, 'password' => $password],
+            function ($m) use ($partenaire, $email) {
+                $subject = "Votre Espace Partenaire est prêt";
+                // $m->from('MAIL_USERNAME', 'jrb.youssef@gmail.com');
+                $m->to($email->email, $partenaire->name)->subject($subject);
+            }
+        );
+    }
 }

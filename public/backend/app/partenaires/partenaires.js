2b58109745bafc7f55d80e4b354d637222509b10
LZ-ouday <o.lazaar@armonya.fr>
2019-10-28 16:37:21 +0100

    delete email js

--- public/backend/app/partenaires/partenaires.js
+++ public/backend/app/partenaires/partenaires.js
@@ -1,11 +1,12 @@
-// When press on delete btn
+// When press on delete btn (Partenaire)
 $(window).on('load', function () {
     let CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
 
     //when press delete button
-    $(".delete-btn").click(function(e){
+   $(".delete-btn").click(function(e){
         e.preventDefault();
         let id = $(this).attr("data-id");
+        
         $.ajax({
             url: hostIdentifier() + '/configuration/partenaires/getPartenaire',
             type: 'POST',
@@ -25,13 +26,62 @@ $(window).on('load', function () {
         });
     })
 
-
     // When confirm delete
     $("#delete").click(function(e){
         e.preventDefault();
         id = $("#PartenaireModal").html();
         $.ajax({
             url:  hostIdentifier() + '/configuration/partenaires/delete',
+            type: 'POST',
+            data: {
+                _token : CSRF_TOKEN,
+                id : id
+            },
+            dataType: 'JSON',
+            beforeSend: function() {
+                $("body").addClass("loading");
+            },
+            success: function (data) {
+                location.reload();
+            }
+        });
+    })
+
+    //================================================================//
+    
+    // When press on delete btn (PartenaireEmail)
+
+
+    //when press delete button
+    $(".delete-btn").click(function(e){
+        e.preventDefault();
+        let id = $(this).attr("data-id");
+        $.ajax({
+            url: hostIdentifier() + '/configuration/partenaires/getEmail',
+            type: 'POST',
+            data: {
+                _token: CSRF_TOKEN,
+                id: id
+            },
+            dataType: 'JSON',
+            success: function (email) {
+                $("#name-Email-to-delete").empty();
+                
+                $("#name-email-to-delete").append(email.id + " - " + email.email)
+                $("#EmailModal").empty()
+                $("#EmailModal").append(email.id)
+                
+            }
+        });
+    })
+
+     // When confirm delete email
+     $("#delete").click(function(e){
+        e.preventDefault();
+        id = $("#EmailModal").html();
+        console.log(id);
+        $.ajax({
+            url:  hostIdentifier() + '/configuration/partenaires/delete/email',
             type: 'POST',
             data: {
                 _token : CSRF_TOKEN,
